eddc3aa5cebabe2f12dc403fbcc8177e8b7f3a02
Alex Dolia <aleksey.dolia@gmail.com>
2017-08-02 16:32:38 +0300

    base c stage class was updated

--- include/stageobject.h
+++ include/stageobject.h
@@ -4,11 +4,28 @@
 #include <QtQuick/QQuickItem>
 
 /**
+ * @brief Moving direction related to rotation property in degrees
+ **/
+enum MovingDirection {
+    MOVING_UP,
+    MOVING_RIGHT = 90,
+    MOVING_DOWN  = 180,
+    MOVING_LEFT  = 270,
+    MOVING_NONE  = 360
+};
+
+/**
  *  @brief The StageObject class is a base class for each visual object (Item)
  **/
 class StageObject : public QQuickItem
 {
     Q_OBJECT
+    Q_PROPERTY(QString imgPath READ getImgPath CONSTANT)
+    Q_PROPERTY(int direction READ getDirection WRITE setDirection NOTIFY directionChanged)
+    Q_PROPERTY(int speed READ getSpeed WRITE setSpeed NOTIFY speedChanged)
+    Q_PROPERTY(bool alive READ isAlive WRITE setLiveStatus NOTIFY liveStatusChanged)
+    Q_PROPERTY(bool moving READ isMoving WRITE setMoveStatus)
+
     /* Copy constructor */
     StageObject(const StageObject &) = default;
     StageObject & operator = (const StageObject &) { return *this; }
@@ -18,18 +35,50 @@ protected:
     StageObject(QQuickItem *parent = Q_NULLPTR);
 
 public:
-    /* Moving direction */
-    enum MovingDirection {
-        MOVING_UP,
-        MOVING_DOWN,
-        MOVING_LEFT,
-        MOVING_RIGHT,
-        MOVING_NONE
-    };
+    /* Path to object's image  getter & setter */
+    virtual QString getImgPath() const{ return this->_img_path; }
+    void setImgPath(const QString& path) { this->_img_path = path; }
+
+    /* Object moving direction getter & setter */
+    int getDirection() const { return this->direction; }
+    void setDirection(int new_direction);
+
+    /* Object speed getter $ setter */
+    int getSpeed() const { return this->speed; }
+    void setSpeed(int new_speed);
+
+    /* Object live status getter & setter */
+    bool isAlive() const { return this->_alive.load(); }
+    void setLiveStatus(const bool live_status);
+
+    /* Object moving status getter & setter */
+    bool isMoving() const { return this->_moving.load(); }
+    void setMoveStatus(bool move_status) { this->_moving = move_status; }
+
+    /* Setters for x & y position
+     * Note: Need to call basic QQuickItem setX and setY inside
+     */
+    virtual void setPosX(qreal);
+    virtual void setPosY(qreal);
+
 
 signals:
+    /* Signals are sent when changes should be propagated to QML */
+    void directionChanged(int);
+    void speedChanged(int);
+    void liveStatusChanged(const bool);
 
-public slots:
+protected:
+    /* Object moving direction in degrees */
+    int direction;
+    int speed;
+
+private:
+    /* Object image path */
+    QString _img_path;
+    /* use atomic */
+    std::atomic<bool> _alive;
+    std::atomic<bool> _moving;
 };
 
 #endif // STAGEOBJECT_H
--- src/stageobject.cpp
+++ src/stageobject.cpp
@@ -1,6 +1,54 @@
 #include "include/stageobject.h"
 
-StageObject::StageObject(QQuickItem *parent) : QQuickItem(parent)
+StageObject::StageObject(QQuickItem *parent) : QQuickItem(parent),
+    _alive(true),
+    _moving(false)
 {
 
+}
+/**
+ * @brief set Object moving direction
+ * @param new_direction
+ * @return void
+ */
+void StageObject::setDirection(int new_direction){
+    if(this->direction != new_direction){
+        this->direction = new_direction;
+        emit directionChanged(new_direction);
+    }
+}
+
+/**
+ * @brief Set object speed value
+ * @param new_speed
+ */
+void StageObject::setSpeed(int new_speed){
+    this->speed = new_speed;
+    speedChanged(new_speed);
+}
+
+/**
+ * @brief Set object live status
+ * @param live_status
+ * @return void
+ */
+void StageObject::setLiveStatus(const bool live_status){
+    this->_alive = live_status;
+    liveStatusChanged(live_status);
+}
+
+/**
+ * @brief Set Object position by X coordinate
+ * @param x
+ */
+void StageObject::setPosX(qreal x){
+    QQuickItem::setX(x);
+}
+
+/**
+ * @brief Set object position by Y coordinate
+ * @param y
+ */
+void StageObject::setPosY(qreal y){
+    QQuickItem::setY(y);
 }
